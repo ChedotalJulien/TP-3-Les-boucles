7d8c855afcd205fef5de5db161d640455b624fdc
Julien <45992191+ChedotalJulien@users.noreply.github.com>
2019-03-21 14:51:48 +0100

    Add files via upload

---
+++ val1.java
@@ -0,0 +1,23 @@
+
+public class val1 
+{
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+
+
+		while (i <= 100)
+
+		{
+			if (i == 50)
+				System.out.println("moitie");      
+
+			else
+				System.out.println(i); 
+			i++;
+
+		}
+	}
+
+}
